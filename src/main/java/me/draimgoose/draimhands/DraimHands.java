5d2a4266b703ef87ab0c968a472903dd6a7a752a
DraimCiDo <danilaorlov4@gmail.com>
2022-03-27 20:36:57 +0300

    [1.0.1] - Добавление подхвата Мира, Простуды, Болезни - Добавление 2 TODO

--- src/main/java/me/draimgoose/draimhands/DraimHands.java
+++ src/main/java/me/draimgoose/draimhands/DraimHands.java
@@ -1,8 +1,31 @@
 package me.draimgoose.draimhands;
 
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DraimHands extends JavaPlugin {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class DraimHands extends JavaPlugin {
+
+    private List<World> worlds = new ArrayList<>();
+    private List<UUID> colds = new ArrayList<>();
+    private List<UUID> diseases = new ArrayList<>();
+
+    private List<World> getWorlds() {
+        return worlds;
+    }
+
+    // TODO: Простуды
+    private List<UUID> getColds() {
+        return colds;
+    }
+
+    // TODO: Болезни
+    private List<UUID> getDiseases() {
+        return diseases;
+    }
 
     @Override
     public void onEnable() {
